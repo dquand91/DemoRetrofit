2b3fca49d8356b4f1cde1f74e8f380d0d50c6851
dquand91@gmail.com <dquand91@gmail.com>
2018-07-01 21:29:31 +0700

    - First Commit

---
+++ Quanlysinhvien/connect.php
@@ -0,0 +1,13 @@
+<?php 
+
+	$hostname = "localhost";
+	$username = "root";
+	$password = "";
+	$databasename = "quanlysinhvien";
+
+	$connect = mysql_connect($hostname, $username, $password, $databasename);
+	
+	
+
+
+ ?>
---
+++ Quanlysinhvien/uploadhinh.php
@@ -0,0 +1,26 @@
+<?php 
+
+	$file_path = "image/"; // noi chua file upload tư client len server
+
+	// Sau khi upload len, file sẽ nằm trong thư mục "tmp_name" để lấy ra được dữ liệu ra thì dùng biến $_FILES
+	// $_FILES['uploaded_file']['name'] => uploaded_file  là biến chứa file đã update, do mình đặt
+	// name là thuôc tính muốn lấy, ở đây là lấy tên(name).
+	//Có một biến PHP toàn cục là $_FILES. Biến này là một mảng liên hợp và giữ tất cả thông tin liên quan đến file được tải lên. Vì vậy, nếu giá trị gán cho thuộc tính name của input trong form upload là file123, khi đó PHP có thể tạo 5 biến sau:
+
+	//$_FILES['file123']['tmp_name'] − File đã upload trong thư mục tạm thời trên Web Server.
+	//$_FILES['file123']['name'] − Tên thực sự của file đã upload.
+	//$_FILES['file123']['size'] − Kích thước tính theo byte của file đã upload.
+	//$_FILES['file123']['type'] − Kiểu MIME của file đã upload.
+	//$_FILES['file123']['error'] − Mã hóa lỗi liên quan đến file tải lên này.
+	$file_path = $file_path.basename($_FILES['uploaded_file']['name']); 
+
+
+	// Sau đó sẽ đẩy cái file từ thư mục tmp_name vào trong folder lưu trữ đã tạo ở trên
+	if (move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$file_path)) {
+		echo $_FILES['uploaded_file']['name'];
+	} else {
+		echo "Failed";
+	}
+	
+
+ ?>
